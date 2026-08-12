a519fb608fabad8393bc5d22e7ea7964caccd451
Mariam Bouthaich <mariambouthaich@gmail.com>
2026-08-12 12:15:11 +0100

    Add files via upload

---
+++ C07/ft_split.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maboutha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/05 20:35:57 by maboutha          #+#    #+#             */
+/*   Updated: 2026/08/05 20:38:26 by maboutha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	is_sep(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	count_words(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] && is_sep(str[i], charset))
+			i++;
+		if (str[i])
+		{
+			count++;
+			while (str[i] && !is_sep(str[i], charset))
+				i++;
+		}
+	}
+	return (count);
+}
+
+int	word_len(char *str, char *charset)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] && !is_sep(str[len], charset))
+		len++;
+	return (len);
+}
+
+char	*copy_word(char *str, char *charset)
+{
+	char	*word;
+	int		len;
+	int		i;
+
+	len = word_len(str, charset);
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**tab;
+	int		i;
+
+	tab = malloc(sizeof(char *) * (count_words(str, charset) + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (*str)
+	{
+		while (*str && is_sep(*str, charset))
+			str++;
+		if (*str)
+		{
+			tab[i] = copy_word(str, charset);
+			i++;
+			while (*str && !is_sep(*str, charset))
+				str++;
+		}
+	}
+	tab[i] = NULL;
+	return (tab);
+}
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char	**tab;
+	int		i;
+
+	tab = ft_split("Bonjour,je;vais:bien", ",;:");
+	i = 0;
+	while (tab[i])
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+	return (0);
+}*/
